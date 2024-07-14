8ed44c3aed198e5f77532b544e259a0e11e53c26
Viktor Szabo <vszabo@protonmail.com>
2024-07-14 07:21:57 -0400

    Jöjjetek, örvendjünk az Úr előtt

--- family_songbook/run.sh
+++ family_songbook/run.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
+## change directory to family_songbook within the repo
 for f in images/*gif; do
     echo "$f"
     #convert "$f" "${f/%gif/png}"
     convert "$f" "${f%.*}.png"
     rm "$f"
 done
-#pdflatex chords.tex
-#pdflatex lyrics.tex
-#texlua ../src/songidx/songidx.lua toc.sxd toc.sbx
-#pdflatex chords.tex
-#pdflatex lyrics.tex
+## Update index & contents (won't work with emojis)
+# pdflatex chords.tex
+# pdflatex lyrics.tex
+# texlua ../src/songidx/songidx.lua toc.sxd toc.sbx
+# pdflatex chords.tex
+# pdflatex lyrics.tex
 
+## Update contents only
 lualatex chords.tex
 lualatex lyrics.tex
-#texlua ../src/songidx/songidx.lua toc.sxd toc.sbx
-#lualatex chords.tex
-#lualatex lyrics.tex
